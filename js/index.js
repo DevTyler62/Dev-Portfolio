d82af9d3c82ca2b35934fb414345773a165f1c6c
Tyler <smithtj921@gmail.com>
2021-12-19 22:05:25 -0500

    Added dark mode feature

--- js/index.js
+++ js/index.js
@@ -16,3 +16,16 @@ if (navToggle) {
 		});
 	}
 }
+
+document.getElementById("myonoffswitch").addEventListener("click", () => {
+	const check = document.getElementById("myonoffswitch").checked;
+	const root = document.getElementsByTagName("html")[0];
+
+	if (check === true) {
+		console.log("true");
+		root.setAttribute("data-theme", "dark");
+	} else {
+		console.log("false");
+		root.setAttribute("data-theme", "light");
+	}
+});
