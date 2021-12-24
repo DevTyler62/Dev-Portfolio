a9685df38c35833af3a99be8aaee26ada7322a88
Tyler <smithtj921@gmail.com>
2021-12-23 22:31:34 -0500

    Added form now sends emails

--- js/index.js
+++ js/index.js
@@ -27,3 +27,27 @@ document.getElementById("myonoffswitch").addEventListener("click", () => {
 		root.setAttribute("data-theme", "light");
 	}
 });
+
+/*======= Email JS ========*/
+(function () {
+	emailjs.init("user_Prb7PtxGBAxs3XwjPcaGj");
+})();
+
+window.onload = function () {
+	document
+		.getElementById("contact_form")
+		.addEventListener("submit", function (event) {
+			event.preventDefault();
+
+			this.contact_number.value = (Math.random() * 100000) | 0;
+
+			emailjs.sendForm("contact_service", "contact_form", this).then(
+				function () {
+					console.log("Sucess");
+				},
+				function (error) {
+					console.log("Error.. ", error);
+				}
+			);
+		});
+};
